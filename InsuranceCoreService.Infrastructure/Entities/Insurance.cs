d8a9aca17b122b20aa1df8ee1f26db9f62ce1cac
Michel Nickbon <michel.lemponen@gmail.com>
2023-11-19 20:40:42 +0100

    Updated packages

--- InsuranceCoreService.Infrastructure/Entities/Insurance.cs
+++ InsuranceCoreService.Infrastructure/Entities/Insurance.cs
@@ -1,4 +1,8 @@
 ﻿namespace InsuranceCoreService.Infrastructure.Entities;
+
 public class Insurance
 {
+    public Guid InsuranceId { get; set; }
+
+    public string? InsuranceNumber { get; set; }
 }
