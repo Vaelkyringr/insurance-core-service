75e1542a3589e42817620b64d8d5c355a066457c
Michel Nickbon <michel.lemponen@gmail.com>
2024-02-14 12:47:19 +0100

    Adjustments to insurance object

--- InsuranceCoreService.Domain/InsuranceAggregate/Insurance.cs
+++ InsuranceCoreService.Domain/InsuranceAggregate/Insurance.cs
@@ -5,26 +5,15 @@ using InsurerAggregate;
 
 public class Insurance : EntityBase
 {
-    public Insurance()
-    {
-        Coverages = new List<Coverage>();
-    }
+    public Insurance() { }
 
-    public Insurance(string insuranceNumber, decimal yearlyPremium)
-    {
-        YearlyPremium = yearlyPremium;
-        InsuranceNumber = insuranceNumber;
-        Created = DateTime.UtcNow;
-        Coverages = new List<Coverage>();
-    }
+    public string InsuranceNumber { get; init; } = null!;
+    public decimal YearlyPremium { get; init; }
+    public int InsurerId { get; init; }
+    public DateTime StartPeriod { get; init; }
+    public DateTime EndPeriod { get; init; }
 
-    public string InsuranceNumber { get; set; } = null!;
-    public decimal YearlyPremium { get; set; }
-    public int InsurerId { get; set; }
-    public DateTime StartPeriod { get; set; }
-    public DateTime EndPeriod { get; set; }
-
-    public Insurer Insurer { get; set; } = null!;
-    public ICollection<Coverage> Coverages { get; set; }
-    public ICollection<InsuranceCoverage> InsuranceCoverages { get; set; } = null!;
+    public Insurer Insurer { get; init; } = null!;
+    public ICollection<Coverage> Coverages { get; init; } = null!;
+    public ICollection<InsuranceCoverage> InsuranceCoverages { get; init; } = null!;
 }
