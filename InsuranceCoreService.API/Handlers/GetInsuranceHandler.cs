1b2f390b6b725a928e8a912aac25523a59e12ff2
Michel Nickbon <michel.lemponen@gmail.com>
2024-02-14 16:47:28 +0100

    Converted to primary constructor

--- InsuranceCoreService.API/Handlers/GetInsuranceHandler.cs
+++ InsuranceCoreService.API/Handlers/GetInsuranceHandler.cs
@@ -4,20 +4,11 @@ using InsuranceCoreService.Domain.InsuranceAggregate;
 
 namespace InsuranceCoreService.API.Handlers;
 
-public class GetInsuranceHandler : IRequestHandler<GetInsuranceByIdQuery, GetInsuranceResponse>
+public class GetInsuranceHandler(IInsuranceRepository repository, IMapper mapper) : IRequestHandler<GetInsuranceByIdQuery, GetInsuranceResponse>
 {
-    private readonly IMapper _mapper;
-    private readonly IInsuranceRepository _repository;
-
-    public GetInsuranceHandler(IInsuranceRepository repository, IMapper mapper)
-    {
-        _mapper = mapper;
-        _repository = repository;
-    }
-
     public async Task<GetInsuranceResponse> Handle(GetInsuranceByIdQuery request, CancellationToken cancellationToken)
     {
-        var insurance = await _repository.GetInsuranceByIdAsync(request.Id);
-        return _mapper.Map<GetInsuranceResponse>(insurance);
+        var insurance = await repository.GetInsuranceByIdAsync(request.Id);
+        return mapper.Map<GetInsuranceResponse>(insurance);
     }
 }
