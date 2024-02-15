d448ceaf180d2ed0ab73eb0376174a06633cbffe
Michel Nickbon <michel.lemponen@gmail.com>
2024-02-15 09:54:56 +0100

    Added value object class

---
+++ InsuranceCoreService.Domain/SeedWork/ValueObject.cs
@@ -0,0 +1,59 @@
+﻿namespace InsuranceCoreService.Domain.SeedWork;
+
+public abstract class ValueObject
+{
+    private static bool EqualOperator(ValueObject left, ValueObject? right)
+    {
+        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+        {
+            return false;
+        }
+        return left?.Equals(right) != false;
+    }
+
+    protected static bool NotEqualOperator(ValueObject left, ValueObject? right)
+    {
+        return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetAtomicValues();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+        using var thisValues = GetAtomicValues().GetEnumerator();
+        using var otherValues = other.GetAtomicValues().GetEnumerator();
+
+        while (thisValues.MoveNext() && otherValues.MoveNext())
+        {
+            if (thisValues is { Current: null } ^ ReferenceEquals(otherValues.Current, null))
+            {
+                return false;
+            }
+
+            if (thisValues.Current != null && !thisValues.Current.Equals(otherValues.Current))
+            {
+                return false;
+            }
+        }
+
+        return !thisValues.MoveNext() && !otherValues.MoveNext();
+    }
+
+    public override int GetHashCode()
+    {
+        return GetAtomicValues()
+            .Select(x => x.GetHashCode())
+            .Aggregate((x, y) => x ^ y);
+    }
+
+    public ValueObject? GetCopy()
+    {
+        return MemberwiseClone() as ValueObject;
+    }
+}
