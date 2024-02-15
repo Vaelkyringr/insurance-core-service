416a6897da97f712ca9d2c3251138c7f3c492866
Michel Nickbon <michel.lemponen@gmail.com>
2024-02-15 13:32:50 +0100

    Added controller tests for Insurer

--- InsuranceCoreService.Tests/API/InsurerControllerTests.cs
+++ InsuranceCoreService.Tests/API/InsurerControllerTests.cs
@@ -19,34 +19,52 @@ public class InsurerControllerTests
     }
 
     [Fact]
-    public async Task GetAllInsurersAsync_ReturnsOkResult_WhenModelStateIsValid()
+    public async Task GetAllInsurersAsync_ReturnsOk_WhenModelStateIsValid()
     {
-        // Arrange
         var query = new GetInsurers();
         var response = new GetInsurersResponse();
+
         _mediatorMock.Setup(m => m.Send(query, default)).ReturnsAsync(response);
 
-        // Act
         var result = await _controller.GetAllInsurersAsync(query);
-
-        // Assert
         var okResult = Assert.IsType<OkObjectResult>(result);
+
         Assert.Same(response, okResult.Value);
     }
 
     [Fact]
-    public async Task CreateInsurerAsync_ReturnsCreatedAtActionResult_WhenModelStateIsValid()
+    public async Task GetAllInsurersAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
     {
-        // Arrange
+        var query = new GetInsurers { PageIndex = 0, PageSize = 0 };
+        _controller.ModelState.AddModelError("PageIndex", "PageIndex must be greater than 0");
+        _controller.ModelState.AddModelError("PageSize", "PageSize must be greater than 0");
+
+        var result = await _controller.GetAllInsurersAsync(query);
+
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task CreateInsurerAsync_ReturnsCreatedAtAction_WhenModelStateIsValid()
+    {
         var command = new CreateInsurer();
         var response = new CreateInsurerResponse { Id = 1 };
         _mediatorMock.Setup(m => m.Send(command, default)).ReturnsAsync(response);
 
-        // Act
+        var result = await _controller.CreateInsurerAsync(command);
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+
+        Assert.Same(response, createdAtActionResult.Value);
+    }
+
+    [Fact]
+    public async Task CreateInsurerAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+    {
+        var command = new CreateInsurer();
+        _controller.ModelState.AddModelError("Name", "The Name field is required.");
+
         var result = await _controller.CreateInsurerAsync(command);
 
-        // Assert
-        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
-        Assert.Same(response, createdAtActionResult.Value);
+        Assert.IsType<BadRequestObjectResult>(result);
     }
 }
